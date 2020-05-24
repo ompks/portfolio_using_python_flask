8518560ccf1e286810178f8d79d06f8b13006481
root <root@ompks.localdomain>
2020-05-24 21:37:06 +0530

    changes

---
+++ Server.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, url_for, redirect
+app = Flask(__name__)
+
+@app.route('/')
+def my_home():
+    return render_template('index.html')
+
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data= request.form.to_dict()
+        print(data)
+        return redirect('thankyou.html')
+    else:
+        return 'something went wrong. Try again!'
